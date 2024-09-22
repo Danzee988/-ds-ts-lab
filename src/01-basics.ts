b914bfd6ef67ffb7c14205035d70bc22ee7c4427
Daniel-Wolski <20098120@mail.wit.ie>
2024-09-22 13:54:44 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,10 +1,27 @@
-const friend1 = {
+interface Friend {
+    name: string;
+    phone: string;
+    age: number
+}
+
+interface ContactInfo {
+    email: string;
+    extension: number;
+  }
+  
+  interface Colleague {
+    name: string;
+    department: string;
+    contact: ContactInfo;
+  }
+
+const friend1: Friend = {
     name: "David Beres",
     phone: "087-12345",
     age: 25,
   };
   
-  const friend2 = {
+  const friend2: Friend = {
     name: "Jan Biernacki",
     phone: "086--12345",
     age: 31,
@@ -14,7 +31,7 @@ const friend1 = {
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -23,7 +40,7 @@ const friend1 = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -32,7 +49,7 @@ const friend1 = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
